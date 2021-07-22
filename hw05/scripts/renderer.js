dde6f04c675994ff010fc005ba1f71b83eadb875
Mashacherry <75532907+Mashacherry@users.noreply.github.com>
2021-07-22 11:06:14 +0300

    Update renderer.js

--- hw05/scripts/renderer.js
+++ hw05/scripts/renderer.js
@@ -5,16 +5,22 @@ let renderer = {
     },
     generateBoard() {
         let flagx = true;
-          let board = '';
+        let board = '';
+        board+='<tr><td></td><td>A</td><td>B</td><td>C</td><td>D</td><td>E</td><td>F</td><td>G</td><td>H</td></tr>'
         for (let y = 0; y < config.rowsCount; y++) {
             board += '<tr>';
             for (let x = 0; x < config.colsCount; x++) {
+                if (x==0){
+                    board += `<td>${y+1}</td>`
+                } 
                 if (flagx) {
                     board += `<td class="black" data-x="${x}" data-y="${y}"</td>`
                 } else  {
                     board += `<td class ="white" data-x="${x}" data-y="${y}"</td>`
                 }
+
                 flagx = !flagx;
+
             }
             board += '</tr>';
             flagx = !flagx;
